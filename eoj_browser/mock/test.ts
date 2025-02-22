69ff1094bbe892a2fc9d3440505a44830b92a8c0
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-02-23 01:32:15 +0530

    ''

--- eoj_browser/mock/test.ts
+++ eoj_browser/mock/test.ts
@@ -3,7 +3,7 @@ import { MockMethod } from "vite-plugin-mock";
 
 export default [
   {
-    url: "/snapshots/files",
+    url: "/list/files",
     method: "get",
     response: () => {
       return [
@@ -18,17 +18,10 @@ export default [
     },
   },
   {
-    url: "/snapshots/versions/test/test.ipynb/1700662570.843829",
+    url: "/list/versions/test/test.ipynb/1700662570.843829",
     method: "get",
     response: () => {
-      return ["0", "1"];
+      return [{ file_name: "1700662568.828779" }, { file_name: "1700662569.331969" }];
     },
-  },
-  {
-    url: "/snapshots/versions/test/test.ipynb/1700662572.585462",
-    method: "get",
-    response: () => {
-      return ["x", "y"];
-    },
-  },
+  }
 ] as MockMethod[];
