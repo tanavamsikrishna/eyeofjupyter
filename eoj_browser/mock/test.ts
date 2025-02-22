1561bce7708d0042207d21ca5958911a086ee6a6
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-02-23 01:32:54 +0530

    ''

--- eoj_browser/mock/test.ts
+++ eoj_browser/mock/test.ts
@@ -8,12 +8,7 @@ export default [
     response: () => {
       return [
         "test/test.ipynb/1700662570.843829",
-        "test/test.ipynb/1700662572.585462",
-        "test/test.ipynb/1700662570.29712",
-        "test/test.ipynb/1700662568.828779",
-        "test/test.ipynb/1700825772.119094",
-        "test/test.ipynb/1700662569.832397",
-        "test/test.ipynb/1700662569.331969",
+        "test/test.ipynb/1700662572.585462"
       ];
     },
   },
